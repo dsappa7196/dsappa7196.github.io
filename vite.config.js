b6eb0e12d77e1c5f67d288307a8cb2d7eef9fc23
Padmasree Sappa <your-email@example.com>
2025-06-05 21:30:49 -0700

    fix: remove base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dsappa7196.github.io/', // <-- this is important!
-  plugins: [react()],
+  plugins: [react()]
 });
